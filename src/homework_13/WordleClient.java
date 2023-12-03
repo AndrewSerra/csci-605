2fc1024656d8737c70e840deb78373476bf2086a
Andrew Serra <acs8929@rit.edu>
2023-12-03 13:21:25 +0300

    gameplay with one player complete

--- src/homework_13/WordleClient.java
+++ src/homework_13/WordleClient.java
@@ -1,104 +1,132 @@
 package homework_13;
 
 import java.io.*;
-import java.util.Random;
+import java.net.Socket;
+import java.util.Arrays;
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
-public class Wordle {
-    static int soManyWordToPLayWith = 0;
-    static final String[] theWords = new String[10231];
-    static final Scanner readGuess = new Scanner(System.in);
+public class WordleClient {
 
-    public static void printArray() {
-        int counter = 0;
-        while (theWords[counter++] != null)
-            System.out.println(theWords[counter - 1]);
+    private Socket socket;
+    private final String host;
+    private final int port;
+    private final Scanner userInput = new Scanner(System.in);
+
+    public WordleClient(String _host, int _port) {
+        host = _host;
+        port = _port;
     }
 
-    public static void readWordsFromFile(String fileName) {
-        try (
-                BufferedReader input = new BufferedReader(new FileReader(fileName));
-        ) {
-            String word;
-            int counter = 0;
-            while ((theWords[counter++] = input.readLine()) != null)
-                soManyWordToPLayWith++;
+    private void connectToGameServer() {
+        try {
+            socket = new Socket(host, port);
         } catch (Exception e) {
-            System.out.println("ExceptionType occurred: " + e.getMessage());
+            System.out.println("Error connecting to socket.");
+            System.out.println(e.getMessage());
+            System.exit(1);
         }
     }
 
-    public static String readUserInput() {
-        String guess = "";
-        System.out.print("> ");
-        if (readGuess.hasNext()) {
-            guess = readGuess.nextLine();
-        }
-        return guess;
-    }
-
-    //	abcdef
-    //	acxxxx
-    //	*_xxxx
-    public static String analyse(String theWordToShow, String guess) {
-        String rValue = "";
-        for (int index = 0; index < guess.length(); index++) {
-            char charAtPositionIndex = guess.charAt(index);
-            if (charAtPositionIndex == theWordToShow.charAt(index))
-                rValue += "*";
-            else if (theWordToShow.indexOf(charAtPositionIndex) > 0)
-                rValue += "_";
-            else
-                rValue += "x";
-
-        }
-        return rValue;
-
-    }
-
-    public static boolean isItSolved(String progressSoFar) {
-        int counter = 0;
-        for (int index = 0; index < progressSoFar.length(); index++) {
-            if (progressSoFar.charAt(index) == '*')
-                counter++;
-        }
-        return progressSoFar.length() == counter;
-    }
-
-    public static String getWord() {
-        return theWords[new Random().nextInt(soManyWordToPLayWith)];
-    }
-
-    public static void playWordle() {
+    private static void printInstructions() {
         System.out.println("_ indicates the letter is in the word but in the wrong spot.");
         System.out.println("* indicates the letter is in the word and correct spot.");
         System.out.println("x indicates the letter is not in the word.");
         System.out.println("Try to guess the word in 5 tries.");
-        int counter = 0;
-        String theWordToGuess = getWord();
-        String theWordToShow = theWordToGuess.replaceAll(".", ".");
-        // System.out.println(theWordToGuess);
-        do {
-            counter++;
-            System.out.print(counter);
-            String theGuess;
-            do {
-                theGuess = readUserInput();
-            } while (theGuess.length() != 5);
-            theWordToShow = analyse(theWordToGuess, theGuess);
-            System.out.println(theGuess);
-            System.out.println(theWordToShow);
-        } while (!isItSolved(theWordToShow) && (counter < 6));
-
-        if (isItSolved(theWordToShow))
-            System.out.println("well done.");
-        else
-            System.out.println("!well done.");
     }
 
-    public static void main(String args[]) {
-        readWordsFromFile("5_char_word.txt");
-        // printArray();
-        playWordle();
+    private String readUserInput() {
+        String guess = "";
+        do {
+            System.out.print(" > ");
+            if  ( userInput.hasNext() )	{
+                guess = userInput.nextLine();
+            }
+        } while ( guess.length() != 5 );
+        return guess;
+    }
+
+    private boolean askForReplay() {
+        boolean continueGame = false;
+        String response = "";
+
+        do {
+            System.out.print("Do you want to play another game? (y/n) ");
+            if(userInput.hasNext()) {
+                response = userInput.nextLine();
+            }
+        } while(!Pattern.matches("^[yn]$", response));
+
+        if(response.equals("y")) {
+            continueGame = true;
+        }
+        return continueGame;
+    }
+
+    private void playOneRound(BufferedReader in, PrintWriter out) throws IOException {
+        String line;
+        int guessCount = 0;
+        boolean isRoundCompleted = false;
+
+        label:
+        while(!isRoundCompleted) {
+            String userGuess = readUserInput();
+            String serverMsg = String.format("user_guess:%s", userGuess);
+            out.println(serverMsg);
+            line = in.readLine();
+            String[] lineComponents = line.split(":");
+            switch (lineComponents[0]) {
+                case "success":
+                    System.out.println("Correct guess. Word found!");
+                    isRoundCompleted = true;
+                    break;
+                case "failed_attempt":
+                    System.out.printf("Result: %s\tGuess Number: %d\n",
+                            lineComponents[1], (++guessCount));
+                    break;
+                case "game_fail":
+                    System.out.println("Reached maximum attempts. Failed.");
+                    isRoundCompleted = true;
+                    break;
+            }
+        }
+    }
+
+    private void play() {
+        connectToGameServer();
+        try (
+                BufferedReader in = new BufferedReader(
+                        new InputStreamReader(socket.getInputStream()));
+                PrintWriter out = new PrintWriter(socket.getOutputStream(), true)) {
+            printInstructions();
+
+            while(true) {
+                String line = in.readLine();
+                if(line == null) break;
+                if(!line.equals("start_game")) {
+                    continue;
+                }
+                System.out.println("Starting game!");
+                playOneRound(in, out);
+                boolean goingToReplay = askForReplay();
+
+                if(!goingToReplay) {
+                    out.println("replay:false");
+                    System.out.println("Thank you playing. Bye!");
+                    break;
+                }
+                out.println("replay:true");
+            }
+        } catch (Exception e) {
+            System.out.println("Error with connection transactions.");
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        String host = "localhost";
+        int port = 1234;
+        new WordleClient(host, port).play();
     }
 }
--- src/homework_13/WordleSinglePlayerServer.java
+++ src/homework_13/WordleSinglePlayerServer.java
@@ -1,4 +1,164 @@
 package homework_13;
 
-public class WordleServer {
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Arrays;
+import java.util.Random;
+
+public class WordleSinglePlayerServer {
+    int port = 1234;
+    int guessCount = 0;
+    boolean didGameEnd = false;
+    boolean shouldGameRestart = true;
+    static int soManyWordToPLayWith = 0;
+    static final String[] theWords = new String[10231];
+    static final int MAX_GUESS_COUNT = 5;
+
+    public WordleSinglePlayerServer(String filename) {
+        readWordsFromFile(filename);
+    }
+
+    public WordleSinglePlayerServer(String filename, int _port) {
+        readWordsFromFile(filename);
+        port = _port;
+    }
+
+    public void readWordsFromFile(String fileName) {
+        try (
+                BufferedReader input = new BufferedReader(new FileReader(fileName));
+        ) {
+            int counter = 0;
+            System.out.println("Reading words from file...");
+            while ((theWords[counter++] = input.readLine()) != null)
+                soManyWordToPLayWith++;
+        } catch (Exception e) {
+            System.out.println("ExceptionType occurred: " + e.getMessage());
+        }
+    }
+
+    //	abcdef
+    //	acxxxx
+    //	*_xxxx
+    public String analyze(String theWordToShow, String guess) {
+        StringBuilder rValue = new StringBuilder();
+        for (int index = 0; index < guess.length(); index++) {
+            char charAtPositionIndex = guess.charAt(index);
+            if (charAtPositionIndex == theWordToShow.charAt(index))
+                rValue.append("*");
+            else if (theWordToShow.indexOf(charAtPositionIndex) > 0)
+                rValue.append("_");
+            else
+                rValue.append("x");
+
+        }
+        return rValue.toString();
+
+    }
+
+    public boolean isItSolved(String progressSoFar) {
+        int counter = 0;
+        for (int index = 0; index < progressSoFar.length(); index++) {
+            if (progressSoFar.charAt(index) == '*')
+                counter++;
+        }
+        return progressSoFar.length() == counter;
+    }
+
+    public String getWord() {
+        System.out.println("Getting a word...");
+        return theWords[new Random().nextInt(soManyWordToPLayWith)];
+    }
+
+    private void resetGame() {
+        guessCount = 0;
+    }
+
+    private void processRestartCmd(BufferedReader in) throws IOException {
+        String[] replayRes = in.readLine().split(":");
+        shouldGameRestart = !replayRes[1].equals("false");
+    }
+
+    private void runOneRound(BufferedReader in, PrintWriter out) throws IOException {
+        System.out.println("Starting new game...");
+        String targetWord = getWord();
+        System.out.println("Game word: " + targetWord);
+        out.println("start_game");
+
+        while (!didGameEnd) {
+            // Being gameplay
+            if(guessCount == (MAX_GUESS_COUNT-1)) {
+                resetGame();
+                out.println("game_fail");
+                didGameEnd = true;
+                processRestartCmd(in);
+                continue;
+            }
+            String line = in.readLine();
+            if (line == null) {
+                break;
+            } else {
+                String[] lineComponents = line.split(":");
+                String cmd = lineComponents[0], msg = lineComponents[1];
+
+                if(cmd.equals("user_guess")) {
+                    String feedbackStr = analyze(targetWord, msg);
+                    boolean isSolved = isItSolved(feedbackStr);
+                    System.out.println(cmd + " " + feedbackStr + " " + isSolved);
+                    if(!isSolved) {
+                        guessCount++;
+                        out.println(String.format("failed_attempt:%s", feedbackStr));
+                    } else {
+                        resetGame();
+                        didGameEnd = true;
+                        out.println(String.format("success:%s", feedbackStr));
+                    }
+                }
+            }
+        }
+        processRestartCmd(in);
+    }
+
+    public void run() {
+        try {
+            ServerSocket server = new ServerSocket(port);
+            System.out.printf("Server starting: %s\n", server);
+            System.out.println("Waiting for a connection...");
+            Socket socket = server.accept();
+            System.out.printf(
+                    "New active socket connection: %s\n", socket.toString());
+            try (
+                    BufferedReader in = new BufferedReader(
+                            new InputStreamReader(socket.getInputStream()));
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(),
+                            true) ) {
+                do {
+                    didGameEnd = false;
+                    runOneRound(in, out);
+                } while(shouldGameRestart);
+            } catch (Exception e) {
+                System.out.println("Error with connection transactions.");
+                System.out.println(e.getMessage());
+                System.exit(3);
+            }
+            socket.close();
+            server.close();
+        } catch (Exception e) {
+            System.out.println("Server cannot be started. Exiting...");
+            System.err.println(e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public static void main(String[] args) {
+        if(args.length != 1) {
+            System.out.println("Incorrect number of arguments.");
+            System.out.println("Usage: java WordleSinglePlayerServer " +
+                    "<filename>");
+            System.exit(2);
+        }
+        WordleSinglePlayerServer wordleServer =
+                new WordleSinglePlayerServer(args[0]);
+        wordleServer.run();
+    }
 }
